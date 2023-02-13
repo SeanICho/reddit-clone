97d965dc310241ee8c92598c5b1a11129ab1cc65
Sean Cho <ihcho131313@gmail.com>
2023-02-13 06:40:12 -0600

    working version after setting up navbar skeleton

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+
+const Navbar:React.FC = () => {
+  return (
+  <Flex bg="white" height="44px" padding="6px 12px">
+    <Flex>
+
+    </Flex>
+  </Flex>
+  );
+};
+
+export default Navbar;
